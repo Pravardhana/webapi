18235c4d8d67cd2846191f9f6d93d1889dda64ca
preethi <preethi2525@gmail.com>
2021-04-12 13:37:56 +0530

    commited all

---
+++ SolutionAPI/WebAPI/Controllers/ShopController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ShopController : ApiController
+    {
+        private DbModel db = new DbModel();
+
+        public IEnumerable<shop> Get()
+        {
+            return db.shops.ToList();
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            var entity = db.shops.FirstOrDefault(e => e.Id == id);
+            if (entity != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, entity);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "product with Id " + id.ToString() + "not found");
+            }
+        }
+        
+        public HttpResponseMessage Post([FromBody]shop sh)
+        {
+            try
+            {
+                db.shops.Add(sh);
+                db.SaveChanges();
+                var message = Request.CreateResponse(HttpStatusCode.Created, sh);
+                return message;
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                var entity = db.shops.FirstOrDefault(e => e.Id == id);
+                if (entity == null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "product with Id " + id.ToString() + "not found");
+                }
+                else
+                {
+                    db.shops.Remove(entity);
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]shop sh)
+        {
+            try
+            {
+                var entity = db.shops.FirstOrDefault(e => e.Id == id);
+                if (entity == null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "product with Id " + id.ToString() + "not found to update");
+
+                }
+                else
+                {
+
+                    entity.Name = sh.Name;
+                    entity.Description = sh.Description;
+                    entity.Price = sh.Price;
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                }
+               catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+
+        }
+
+
+        }
+
+    }
+
+
+        
+
+
+
+
